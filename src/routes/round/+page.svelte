08d035f1c72c876af12ba9f4a3c7252b4f0532ed
headmad <kov.ps@ya.ru>
2024-12-26 01:14:31 +0800

    edit rounded colorpicker

--- src/routes/round/+page.svelte
+++ src/routes/round/+page.svelte
@@ -1,99 +1,172 @@
 <script>
-  import {scale} from "$lib";
+  import { scale, clamp } from "$lib";
   import coordinator from "$lib/actions/coordinator.svelte.js";
-  // sqrt((х2-х1)^2+(y2-y1)^2)
+  let size = 300;
+  let half = size / 2;
+  let outerRadius = size / 2;
+  let innerRadius = outerRadius - size / 10;
+  let slSize = Math.floor((innerRadius - 10) * 1.4);
+  let hueOrbit = size / 10 / 2;
 
-  let t = $state('15px');
-  let l = $state('50%');
-  let angle = $state(0);
-  let hue = $state(0);
+  let H = $state(0);
+  let S = $state(100);
+  let L = $state(50);
 
+  let huePicker = $state({
+    top: hueOrbit + "px",
+    left: "50%",
+  });
 
+  let slPicker = $state({
+    top: "50%",
+    left: "50%",
+    xmin: half - slSize/2,
+    ymin: half - slSize/2,
+    xmax: half + slSize/2,
+    ymax: half + slSize/2,
+  });
+
+  let picker = null;
+ 
   const handlers = {
-    onaction({top, left, width, height}) {
-      const sqrt = Math.sqrt((top - height / 2) ** 2 + (left - width / 2) ** 2);
-
-      angle = Math.round(Math.atan2(left - width / 2, height / 2 - top) * 180 / Math.PI);
-
-      hue = Math.round((angle + 180) / 6);
-
+    onstart({ left, top }) {
+      document.body.classList.add("unselectable");
       const dist = Math.sqrt((150 - top) ** 2 + (150 - left) ** 2);
 
-      const k = 135 / dist;
-        l = (left - 150) * k + 150 + 'px';
-        t = (top - 150) * k + 150 + 'px';
+      picker = dist < innerRadius ? slPicker : huePicker;
+    },
 
-        return;
+    onend() {
+      document.body.classList.remove("unselectable");
+      picker = null;
+    },
+    onaction({ top, left, width, height }) {
+      const dist = Math.sqrt((half - top) ** 2 + (half - left) ** 2);
 
-      if (dist < 150 && dist > 100) {
-        t = top + 'px';
-        l = left + 'px';
+      if (picker === slPicker) {
+        const y = clamp(top, picker.ymin, picker.ymax);
+        const x = clamp(left, picker.xmin, picker.xmax);
+        const xL = scale(x, [picker.xmin, picker.xmax], [100, 50]);
 
-      } else if (dist > 150){
-        const k = 150 / dist;
-        l = (left - 150) * k + 150 + 'px';
-        t = (top - 150) * k + 150 + 'px';
+        L = scale(y, [picker.ymin, picker.ymax], [xL, 0]);
 
-      } else {
-        const k = 100 / dist;
-        l = (left - 150) * k + 150 + 'px';
-        t = (top - 150) * k + 150 + 'px';
+        S = scale(x, [picker.xmin, picker.xmax], [0, 100]);
+        picker.top = y + "px";
+        picker.left = x + "px";
       }
-      
-    }
+
+      if (picker === huePicker) {
+        H = Math.round(
+          (Math.atan2(left - half, half - top) * 180) / Math.PI,
+        );
+        const k = (half - hueOrbit) / dist;
+        picker.left = (left - half) * k + half + "px";
+        picker.top = (top - half) * k + half + "px";
+      }
+    },
   };
 </script>
 
-{angle} {hue}
-<div use:coordinator={handlers} style="--angle: {angle}deg">
-  <span style="top: {t}; left: {l}"></span>
+<div
+  class="body"
+  use:coordinator={handlers}
+  style="
+  --angle: {H}deg;
+  --sl-size: {slSize}px;
+  --HUE: hsl({H}, 100%, 50%);
+  --HSL: hsl({H}, {S}%, {L}%);
+  "
+>
+  <svg
+    width="100%"
+    height="100%"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <defs>
+      <mask id="circle">
+        <rect width="100%" height="100%" fill="black" />
+        <circle r="50%" cx="50%" cy="50%" fill="white" />
+        <circle r="40%" cx="50%" cy="50%" fill="black" />
+      </mask>
+    </defs>
+  </svg>
+
+  <div id="circle"></div>
+  <div class="sl"></div>
+  <span class="hue-picker" style="top: {huePicker.top}; left: {huePicker.left}"
+  ></span>
+  <span class="sl-picker" style="top: {slPicker.top}; left: {slPicker.left}"
+  ></span>
 </div>
 
-
-
-<p style="background: hsl({angle}, 100%, 50%)">{angle} {hue}</p>
-
+<p style="background: hsl({H}, {S}%, {L}%)">Hello</p>
 
 <style>
-  div {
-    width: 300px;
-    height: 300px;
-    border-radius: 50%;
-    position: relative;
-    background-image: conic-gradient(
-			red 0,
-			#ff0 16.6%,
-			#0f0 33.2%,
-			#0ff 49.8%,
-			#00f 66.4%,
-			#f0f 83%,
-			red
-		);
-
-    &::before {
-      content: '';
-      position: absolute;
-      top: 30px;
-      left: 30px;
-      right: 30px;
-      bottom: 30px;
-      border-radius: 50%;
-      background-color: #fff;
-    }
-  
-    
+  .sl {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: var(--sl-size);
+    height: var(--sl-size);
+    background-color: var(--HUE);
+    background-image: linear-gradient(0deg, #000, rgba(204, 154, 129, 0)),
+      linear-gradient(90deg, #fff, rgba(255, 255, 255, 0));
+    /* box-shadow: 0 0 3px rgba(0, 0, 0, 0.6); */
+    transform: translate(-50%, -50%);
   }
 
-  span {
+  svg {
+    position: absolute;
+  }
+
+  #circle {
+    width: 100%;
+    height: 100%;
+    mask: url(#circle);
+    /* border-radius: 50%; */
+    background-image: conic-gradient(
+      red 0,
+      #ff0 16.6%,
+      #0f0 33.2%,
+      #0ff 49.8%,
+      #00f 66.4%,
+      #f0f 83%,
+      red
+    );
+  }
+  .body {
+    width: 300px;
+    height: 300px;
+    position: relative;
+  }
+
+  .hue-picker {
     position: absolute;
     width: 2px;
-    height: 34px;
+    height: 10%;
     border: 2px solid #fff;
-		/* width: 10px;
-		height: 10px;
-		border: 2px solid #fff;
-		border-radius: 50%; */
-		transform: translate(-50%, -50%) rotate(var(--angle));
-		box-shadow: 0 0 3px rgba(0, 0, 0, 0.6);
+
+    transform: translate(-50%, -50%) rotate(var(--angle));
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.6);
+  }
+
+  .sl-picker {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    border: 2px solid #fff;
+    border-radius: 50%;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.6);
+    transform: translate(-50%, -50%);
+  }
+
+  :global(.unselectable) {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
   }
 </style>
